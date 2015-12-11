c8e31ae967ebe315979ff0a642a1c59531fe6d92
jovi <huangf@garena.com>
2015-12-11 18:23:58 +0800

    Base component

---
+++ js/mixins/BaseComponent.js
@@ -0,0 +1,27 @@
+/**
+ * Created by labs-huangf-mac on 11/12/15.
+ */
+import React, { Component } from 'react';
+import { is } from 'immutable';
+
+export default class BaseComponent extends Component {
+  constructor(props) {
+    super(props);
+    this._autoBinding.bind(this)();
+  }
+
+  _autoBinding(skipMethods = []) {
+    for (let name of Object.getOwnPropertyNames(Object.getPrototypeOf(this))) {
+      let method = this[name];
+      skipMethods = ['render', 'constructor'].concat(skipMethods);
+      if (!(method instanceof Function) || method === this || skipMethods.indexOf(name) > -1) {
+        continue;
+      }
+      this[name] = method.bind(this);
+    }
+  }
+
+  //shouldComponentUpdate(nextProps, nextState) {
+  //  return !(this.props === nextProps || is(this.props, nextProps)) || !(this.state === nextState || is(this.state, nextState));
+  //}
+}
